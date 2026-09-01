1b0b30e43d66493016c1e439f67c49aee9791bb1
jcmaes <j.maes@hw.ac.uk>
2026-09-01 14:16:39 +0100

    parallel atcher bat in tutorials that are likely run in archer2

---
+++ tutorials/stressAnalysis/solidDisplacementElasticVOSFoam/spheresVOS/parallel_archer2.bat
@@ -0,0 +1,46 @@
+#!/bin/bash
+#SBATCH --job-name=parallel
+#SBATCH --time=0:20:0
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --cpus-per-task=1
+
+# Replace [budget code] below with your budget code (e.g. t01)
+#SBATCH --account=ecseaj02
+#SBATCH --partition=standard
+#SBATCH --qos=standard
+
+# Set the number of threads to 1 to avoid auto-threading
+export OMP_NUM_THREADS=1
+
+# Propagate the cpus-per-task setting from script to srun commands
+export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
+
+# Configure GCF using Archer2 module
+module load other-software
+module load gcfoam/5.2
+source $GCFOAM_DIR/etc/bashrc_archer2
+
+# Configure Python
+module load cray-python
+source /work/ecseaj02/ecseaj02/gavingcf/myvenv/bin/activate
+
+# Exit if the total number of tasks do not equal the total number of processor directories.
+export NP="$(find processor* -maxdepth 0 -type d -print| wc -l)"
+if [ "$NP" -ne "$((SLURM_TASKS_PER_NODE*SLURM_JOB_NUM_NODES))" ]; then
+     echo "Error: number of tasks does not equal the number of processor directories"
+     echo "SBATCH ntasks-per-node times nodes must equal $NP"
+     echo "However they are $SLURM_TASKS_PER_NODE and $SLURM_JOB_NUM_NODES, respectively"
+     exit 1
+fi
+
+export PLATFORM=ARCHER2
+echo -e "Run job_name in parallel on $NP $PLATFORM processors"
+
+# Choose just one of the following parallel scripts.
+#./createMesh.sh
+#./initCaseStress.sh
+#./runCaseStress.sh
+#./processStress.sh
+
+
